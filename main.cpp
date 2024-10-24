f185ca19500f45af1186a3aa7a7524871edf23fb
gioauc <giorgio.auciello@marconibari.edu.it>
2024-10-24 08:15:58 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,19 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+  int totNumeri(0), i(0);
+  float x(0), max(0);
+  cout << "Quanti numeri inserire?" << endl;
+  cin >> totNumeri;
+  for (i = 1; i <= totNumeri; i++) {
+    cout << "Inserisci il " << i << "^ numero" << endl;
+    cin >> x;
+    if (i == 1) {
+      max = x;
+    }
+    if (x > max) {
+      max = x;
+    }
+  }
+  cout << "Il massimo è " << max << endl;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
